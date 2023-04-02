2b8f757864ceac8439cb6f80f4a5d63f961c9bb7
MrSha55 <577227260@qq.com>
2023-04-02 19:41:15 +0800

    test

--- others/scripts.js
+++ others/scripts.js
@@ -27,37 +27,6 @@ function onMarkerLost() {
   initialPopupY = "-100%";
 }
 
-//下划事件
-
-// var initialPopupY;
-
-// // 添加触摸事件处理器
-// function onTouchStart(event) {
-//   initialTouchY = event.touches[0].clientY;
-// }
-
-// function onTouchMove(event) {
-//   var currentTouchY = event.touches[0].clientY;
-//   var deltaY = initialTouchY - currentTouchY;
-
-//   if (deltaY < 0) { // 如果手指向下滑动
-//     var newPopupY = parseFloat(initialPopupY) - deltaY;
-//     popup.style.bottom = `${newPopupY}px`;
-//   }
-// }
-
-
-// // function onTouchEnd(event) {
-// //   var finalTouchY = event.changedTouches[0].clientY;
-// //   var deltaY = initialTouchY - finalTouchY;
-
-// //   if (deltaY < -100) { // 下滑超过100像素
-// //     onMarkerLost();
-// //   } else { // 否则，弹窗返回初始位置
-// //     popup.style.bottom = initialPopupY + "px";
-// //   }
-// // }
-
 
 // DOM 加载完成后执行
 document.addEventListener("DOMContentLoaded", function () {
@@ -66,11 +35,25 @@ document.addEventListener("DOMContentLoaded", function () {
   marker.addEventListener("markerFound", onMarkerFound);
   marker.addEventListener("markerLost", onMarkerLost);
 
-//   // 添加触摸事件监听器
-//   var popup = document.querySelector(".popup");
-//   popup.addEventListener("touchstart", onTouchStart);
-//   popup.addEventListener("touchmove", onTouchMove);
-//   popup.addEventListener("touchend", onTouchEnd);
+   const slides = document.querySelectorAll(".slide");
+  const dots = document.querySelectorAll(".dot");
+
+  function goToSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.style.display = i === index ? "block" : "none";
+    });
+
+    dots.forEach((dot, i) => {
+      dot.classList.toggle("active", i === index);
+    });
+  }
+
+  dots.forEach((dot, i) => {
+    dot.addEventListener("click", () => goToSlide(i));
+  });
+
+  goToSlide(0);
+
 
 });
 
