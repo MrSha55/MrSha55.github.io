b141f6a1c2a7fa7d69fb49ae076a590b9a8c8782
MrSha55 <577227260@qq.com>
2023-04-03 16:17:56 +0800

    test

--- others/scripts.js
+++ others/scripts.js
@@ -16,21 +16,14 @@ function onMarkerFound() {
   let currentIndex = 0;
   let startTouchX = 0;
 
-
-// 在onMarkerFound函数中添加以下代码
-indicators.forEach((indicator, index) => {
-  indicator.addEventListener("click", () => {
-    slider.style.transform = `translateX(-${index * 100}%)`;
-    indicators[currentIndex].classList.remove("active");
-    currentIndex = index;
-    indicators[currentIndex].classList.add("active");
-
-    // 添加以下代码，更新指示器的transform属性
-    const sliderBackground = document.querySelector(".slider-background");
-    sliderBackground.style.transform = `translateX(${currentIndex * (100 / (indicators.length - 1))}%)`;
+  indicators.forEach((indicator, index) => {
+    indicator.addEventListener('click', () => {
+      slider.style.transform = `translateX(-${index * 100}%)`;
+      indicators[currentIndex].classList.remove('active');
+      currentIndex = index;
+      indicators[currentIndex].classList.add('active');
+    });
   });
-});
-
 
   slider.addEventListener('touchstart', (event) => {
     startTouchX = event.touches[0].clientX;
@@ -45,22 +38,12 @@ indicators.forEach((indicator, index) => {
       slider.style.transform = `translateX(-${currentIndex * 100}%)`;
       indicators[currentIndex + 1].classList.remove('active');
       indicators[currentIndex].classList.add('active');
-
-      // 添加以下代码，更新指示器的transform属性
-    const sliderBackground = document.querySelector(".slider-background");
-    sliderBackground.style.transform = `translateX(${currentIndex * (100 / (indicators.length - 1))}%)`;
-
     } else if (delta < -50 && currentIndex < indicators.length - 1) {
       currentIndex++;
       slider.style.transform = `translateX(-${currentIndex * 100}%)`;
       indicators[currentIndex - 1].classList.remove('active');
       indicators[currentIndex].classList.add('active');
-
-      const sliderBackground = document.querySelector(".slider-background");
-      sliderBackground.style.transform = `translateX(${currentIndex * (100 / (indicators.length - 1))}%)`;
     }
-
-
   });
 
 }
