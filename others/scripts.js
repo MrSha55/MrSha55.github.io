3405481af2b78205c867f557e1a5c53d68cc91e2
MrSha55 <577227260@qq.com>
2023-04-03 16:08:50 +0800

    test

--- others/scripts.js
+++ others/scripts.js
@@ -1,4 +1,3 @@
-var initialPopupY = "-100%";
 
 function onMarkerFound() {
   // 显示图片
@@ -6,7 +5,7 @@ function onMarkerFound() {
   imageEntity.setAttribute("visible", true);
 
   // 显示弹窗
-  var popup = imageEntity.querySelector(".popup");
+  var popup = document.querySelector(".popup");
   popup.classList.add("open");
 
   popup.style.bottom = "0%";
@@ -17,14 +16,21 @@ function onMarkerFound() {
   let currentIndex = 0;
   let startTouchX = 0;
 
-  indicators.forEach((indicator, index) => {
-    indicator.addEventListener('click', () => {
-      slider.style.transform = `translateX(-${index * 100}%)`;
-      indicators[currentIndex].classList.remove('active');
-      currentIndex = index;
-      indicators[currentIndex].classList.add('active');
-    });
+
+// 在onMarkerFound函数中添加以下代码
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener("click", () => {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+    indicators[currentIndex].classList.remove("active");
+    currentIndex = index;
+    indicators[currentIndex].classList.add("active");
+
+    // 添加以下代码，更新指示器的transform属性
+    const sliderBackground = document.querySelector(".slider-background");
+    sliderBackground.style.transform = `translateX(${currentIndex * (100 / (indicators.length - 1))}%)`;
   });
+});
+
 
   slider.addEventListener('touchstart', (event) => {
     startTouchX = event.touches[0].clientX;
@@ -39,28 +45,45 @@ function onMarkerFound() {
       slider.style.transform = `translateX(-${currentIndex * 100}%)`;
       indicators[currentIndex + 1].classList.remove('active');
       indicators[currentIndex].classList.add('active');
+
+      // 添加以下代码，更新指示器的transform属性
+  const sliderBackground = document.querySelector(".slider-background");
+  sliderBackground.style.transform = `translateX(${currentIndex * (100 / (indicators.length - 1))}%)`;
+
     } else if (delta < -50 && currentIndex < indicators.length - 1) {
       currentIndex++;
       slider.style.transform = `translateX(-${currentIndex * 100}%)`;
       indicators[currentIndex - 1].classList.remove('active');
       indicators[currentIndex].classList.add('active');
+
+       const sliderBackground = document.querySelector(".slider-background");
+  sliderBackground.style.transform = `translateX(${currentIndex * (100 / (indicators.length - 1))}%)`;
     }
+
+
   });
 
-  const closePopupButton = popup.querySelector('.close-popup');
-  closePopupButton.addEventListener('click', () => {
-    popup.classList.remove('open');
-  });
 }
 
 function onMarkerLost() {
+  console.log("Marker lost");
   // 隐藏图片
   var imageEntity = document.querySelector("#image-entity");
   imageEntity.setAttribute("visible", false);
 
   // 隐藏弹窗
-  var popup = imageEntity.querySelector(".popup");
+  var popup = document.querySelector(".popup");
   popup.classList.remove("open");
 
-  popup.style.bottom = initialPopupY;
+  popup.style.bottom = "-100%";
+  initialPopupY = "-100%";
 }
+
+
+// DOM 加载完成后执行
+document.addEventListener("DOMContentLoaded", function () {
+  // 为 marker 绑定事件
+  var marker = document.querySelector("a-marker");
+  marker.addEventListener("markerFound", onMarkerFound);
+  marker.addEventListener("markerLost", onMarkerLost);
+});
